439bbec2f9da48676cc6dfcb90f6dddb01000aac
Marcos Nolasco <marcosabn08@gmail.com>
2024-04-22 14:44:21 -0300

    create todo route and test

--- backend/app/routers/todos.py
+++ backend/app/routers/todos.py
@@ -1,3 +1,35 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database import get_session
+from app.models import Todo, User
+from app.schemas import TodoPublic, TodoSchema
+from app.security import get_current_user
+
+router = APIRouter()
+
+CurrentSession = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
 
 router = APIRouter(prefix='/todos', tags=['todos'])
+
+
+@router.post('/', response_model=TodoPublic)
+def create_todo(
+    todo: TodoSchema,
+    user: CurrentUser,
+    session: CurrentSession,
+):
+    db_todo: Todo = Todo(
+        title=todo.title,
+        description=todo.description,
+        state=todo.state,
+        user_id=user.id,
+    )
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
+    return db_todo
