944fb935c319b3b7271fbbd67dfe06f1f7af2f03
Marcos Nolasco <marcosabn08@gmail.com>
2024-04-22 22:14:36 -0300

    composing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -39,15 +39,11 @@ def session():
         'sqlite:///:memory:',
         connect_args={'check_same_thread': False},
         poolclass=StaticPool,
-    )  # cria um mecanismo SQLite db em memória usando SQLAlchemy.
-    Session = sessionmaker(
-        bind=engine
-    )  # fábrica de sessões para conexão com banco de dados
-    Base.metadata.create_all(engine)  # cria todas as tabelas
-    yield Session()  # persiste e propaga ums instancia de session
-    Base.metadata.drop_all(
-        engine
-    )  # no final do teste, apaga as tabelas em mémoria
+    )
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    yield Session()
+    Base.metadata.drop_all(engine)
 
 
 @pytest.fixture()
